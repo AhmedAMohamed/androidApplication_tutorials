cfa13ac780f6066adca41a45d188ca289e5179eb
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2016-03-19 23:34:02 +0200

    simple calculator with menus
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- app/src/main/java/com/example/ahmeda/general_app/Calculator.java
+++ app/src/main/java/com/example/ahmeda/general_app/Calculator.java
@@ -3,6 +3,7 @@ package com.example.ahmeda.general_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
@@ -26,9 +27,42 @@ public class Calculator extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getTitle().toString().contains("Mul"))
+        {
+            if(item.getItemId() == R.id.mul_sub_item_menu_1) {
+                resultMainHandler("mul2");
+            }
+            else {
+                resultMainHandler("mul3");
+            }
+            Toast.makeText(getApplicationContext(),"Mul", Toast.LENGTH_SHORT).show();
+        }
+        else if(item.getTitle().toString().contains("Add")) {
+            resultMainHandler("add");
+            Toast.makeText(getApplicationContext(), "Add", Toast.LENGTH_SHORT).show();
+        }
+        else if(item.getTitle().toString().contains("Sub")) {
+            resultMainHandler("sub");
+            Toast.makeText(getApplicationContext(), "Sub", Toast.LENGTH_SHORT).show();
+        }
+        else if(item.getTitle().toString().contains("Di")) {
+            resultMainHandler("div");
+            Toast.makeText(getApplicationContext(), "Di", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        getMenuInflater().inflate(R.menu.result_menu, menu);
+        getMenuInflater().inflate(R.menu.action_menu, menu);
 
     }
 
@@ -36,23 +70,43 @@ public class Calculator extends AppCompatActivity {
     public boolean onContextItemSelected(MenuItem item) {
         if(item.getTitle().toString().contains("Mul"))
         {
-            Toast.makeText(getApplicationContext(),"Mul", Toast.LENGTH_LONG).show();
+
+            Toast.makeText(getApplicationContext(),"Mul", Toast.LENGTH_SHORT).show();
         }
         else if(item.getTitle().toString().contains("Add")) {
-            Toast.makeText(getApplicationContext(), "Add", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Add", Toast.LENGTH_SHORT).show();
         }
         else if(item.getTitle().toString().contains("Sub")) {
-            Toast.makeText(getApplicationContext(), "Sub", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Sub", Toast.LENGTH_SHORT).show();
         }
         else if(item.getTitle().toString().contains("Di")) {
-            Toast.makeText(getApplicationContext(), "Di", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Di", Toast.LENGTH_SHORT).show();
         }
         return super.onContextItemSelected(item);
     }
 
-    private void resultMainHandler() {
+    private void resultMainHandler(String op) {
         if(Double.parseDouble(firstNum.getText().toString()) >= 0 || Double.parseDouble(firstNum.getText().toString()) <= 0) {
+            if(Double.parseDouble(secondNum.getText().toString()) >= 0 || Double.parseDouble(secondNum.getText().toString()) <= 0) {
+                if(op == "mul2") {
+                    result.setText(Double.parseDouble(firstNum.getText().toString()) * Double.parseDouble(secondNum.getText().toString()) * 2 + " ");
+                }
+                if(op == "mul3") {
+                    result.setText(Double.parseDouble(firstNum.getText().toString()) * Double.parseDouble(secondNum.getText().toString()) * 3 + " ");
+                }
+                if(op == "add") {
+                    result.setText(Double.parseDouble(firstNum.getText().toString()) + Double.parseDouble(secondNum.getText().toString())+ " ");
+                }
+                if(op == "sub") {
+                    result.setText(Double.parseDouble(firstNum.getText().toString()) - Double.parseDouble(secondNum.getText().toString()) + " ");
+                }
+                if(op == "div") {
+                    result.setText(Double.parseDouble(firstNum.getText().toString()) / Double.parseDouble(secondNum.getText().toString())  + " ");
+                }
+            }
+            else {
 
+            }
         }
         else {
 
